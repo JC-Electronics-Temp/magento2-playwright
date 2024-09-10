edc84d7bc027448fcfded39863c1ad30813060a2
Arjen Miedema <a.miedema@jc-electronics.nl>
2024-09-10 09:57:30 +0200

    Basic Playwright tests integration

---
+++ tests/homepage.spec.js
@@ -0,0 +1,8 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test.describe('Homepage', () => {
+    test('Search Field', async ({ page }) => {
+        await page.goto(process.env.PLAYWRIGHT_BASE_URL);
+    });
+});

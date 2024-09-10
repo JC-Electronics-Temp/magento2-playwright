c15d5c3e1fda6832b1c0784ee36a3e70baf5b8fb
Arjen Miedema <a.miedema@jc-electronics.nl>
2024-09-10 12:30:32 +0200

    Set a base URL for all tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,10 +25,13 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    /* Ignore https errors if they apply (should only happen on local) */
+    ignoreHTTPSErrors: true,
   },
 
   /* Configure projects for major browsers */
--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -2,6 +2,6 @@ const { test, expect } = require('@playwright/test');
 
 test.describe('Homepage', () => {
     test('Search Field', async ({ page }) => {
-        await page.goto(process.env.PLAYWRIGHT_BASE_URL);
+        await page.goto('/');
     });
 });
